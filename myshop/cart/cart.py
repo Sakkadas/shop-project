28b0f9eb0bd1b253c51d7995afe32ef897e92187
IlyaNesterov <Eildos95@gmail.com>
2021-04-01 21:06:09 +0300

    Formatted code

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -4,6 +4,7 @@ from shop.models import Product
 
 
 class Cart(object):
+
     def __init__(self, request):
         """
         Initialize the cart.
@@ -14,14 +15,37 @@ class Cart(object):
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
+    def __iter__(self):
+        """
+        Iterate over the items in the cart and get the products
+        from the database.
+        """
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def __len__(self):
+        """
+        Count all items in the cart.
+        """
+        return sum(item['quantity'] for item in self.cart.values())
+
     def add(self, product, quantity=1, override_quantity=False):
         """
         Add a product to the cart or update its quantity.
         """
         product_id = str(product.id)
         if product_id not in self.cart:
-            self.cart[product_id] = {
-                'quantity': 0, 'price': str(product.price)}
+            self.cart[product_id] = {'quantity': 0,
+                                      'price': str(product.price)}
         if override_quantity:
             self.cart[product_id]['quantity'] = quantity
         else:
@@ -40,30 +64,9 @@ class Cart(object):
             del self.cart[product_id]
             self.save()
 
-    def __iter__(self):
-        """
-        Iterate over the items in the cart and get the products
-        from the database.
-        """
-        product_ids = self.cart.keys()
-        products = Product.objects.filter(id__in=product_ids)
-        cart = self.cart.copy()
-        for product in products:
-            cart[str(product.id)]['product'] = product
-        for item in cart.values():
-            item['price'] = Decimal(item['price'])
-            item['total_price'] = item['price'] * item['quantity']
-            yield item
-
-    def __len__(self):
-        """
-        Count all items in the cart.
-        """
-        return sum(item['quantity'] for item in self.cart.values())
-
-    def get_total_price(self):
-        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
-
     def clear(self):
         del self.session[settings.CART_SESSION_ID]
         self.save()
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
--- myshop/cart/forms.py
+++ myshop/cart/forms.py
@@ -5,8 +5,6 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(
-        choices=PRODUCT_QUANTITY_CHOICES,
-        coerce=int)
-    override = forms.BooleanField(required=False,
-                                  initial=False,
-                                  widget=forms.HiddenInput)
+        choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
+    override = forms.BooleanField(
+        required=False, initial=False, widget=forms.HiddenInput)
