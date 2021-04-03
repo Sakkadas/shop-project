0e63101df3a75558946952fe6b230f9f568547ef
IlyaNesterov <Eildos95@gmail.com>
2021-04-03 22:16:23 +0300

    Added Celery options

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
-
+from .tasks import order_created
 
 def order_create(request):
     cart = Cart(request)
@@ -16,6 +16,8 @@ def order_create(request):
                                          price=item['price'],
                                          quantity=item['quantity'])
             cart.clear()
+            # launch asynchronous task
+            order_created.delay(order.id)
             return render(request,
                           'orders/order/created.html',
                           {'order': order})
