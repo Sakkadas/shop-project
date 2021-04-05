17d52da84e8338ee641360442ebe59633b5f1b78
IlyaNesterov <Eildos95@gmail.com>
2021-04-05 21:06:42 +0300

    Extended admin

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -4,6 +4,9 @@ from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
 from django.urls import reverse
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
+from .models import Order
 
 
 def order_create(request):
@@ -18,7 +21,7 @@ def order_create(request):
                                          price=item['price'],
                                          quantity=item['quantity'])
             cart.clear()
-             # launch asynchronous task
+            # launch asynchronous task
             order_created.delay(order.id)
             # set the order in the session
             request.session['order_id'] = order.id
@@ -30,3 +33,11 @@ def order_create(request):
     return render(request,
                   'orders/order/create.html',
                   {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request,
+                  'admin/orders/order/detail.html',
+                  {'order': order})
