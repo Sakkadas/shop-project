e99cecfb5657e627ce0f4d6e5b186a9f45c21ccd
IlyaNesterov <Eildos95@gmail.com>
2021-04-06 22:21:59 +0300

    Deleted WeasyPrint

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,10 +1,10 @@
 import csv
 import datetime
-from django.contrib import admin
-from .models import Order, OrderItem
 from django.http import HttpResponse
+from django.contrib import admin
 from django.urls import reverse
 from django.utils.safestring import mark_safe
+from .models import Order, OrderItem
 
 
 class OrderItemInline(admin.TabularInline):
@@ -19,9 +19,11 @@ def export_to_csv(modeladmin, request, queryset):
     response['Content-Disposition'] = content_disposition
     writer = csv.writer(response)
 
-    fields = [field for field in opts.get_fields() if not field.many_to_many
-              and not field.one_to_many]
+    fields = [field for field in opts.get_fields() if not field.many_to_many\
+    and not field.one_to_many]
+    # Write a first row with header information
     writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
     for obj in queryset:
         data_row = []
         for field in fields:
@@ -31,15 +33,12 @@ def export_to_csv(modeladmin, request, queryset):
             data_row.append(value)
         writer.writerow(data_row)
     return response
-
-
 export_to_csv.short_description = 'Export to CSV'
 
 
 def order_detail(obj):
     url = reverse('orders:admin_order_detail', args=[obj.id])
     return mark_safe(f'<a href="{url}">View</a>')
-
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,16 +1,14 @@
-import weasyprint
-from django.shortcuts import render, redirect
-from .models import OrderItem
-from .forms import OrderCreateForm
-from cart.cart import Cart
-from .tasks import order_created
 from django.urls import reverse
+from django.shortcuts import render, redirect
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import get_object_or_404
-from .models import Order
 from django.conf import settings
 from django.http import HttpResponse
-from django.template.loader import render_to_strings
+from django.template.loader import render_to_string
+from cart.cart import Cart
+from .models import OrderItem, Order
+from .forms import OrderCreateForm
+from .tasks import order_created
 
 
 def order_create(request):
@@ -24,6 +22,7 @@ def order_create(request):
                                          product=item['product'],
                                          price=item['price'],
                                          quantity=item['quantity'])
+            # clear the cart
             cart.clear()
             # launch asynchronous task
             order_created.delay(order.id)
@@ -31,7 +30,6 @@ def order_create(request):
             request.session['order_id'] = order.id
             # redirect for payment
             return redirect(reverse('payment:process'))
-
     else:
         form = OrderCreateForm()
     return render(request,
@@ -55,6 +53,6 @@ def admin_order_pdf(request, order_id):
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
     weasyprint.HTML(string=html).write_pdf(response,
-                                           stylesheets=[weasyprint.CSS(
-                                               settings.STATIC_ROOT + 'css/pdf.css')])
+        stylesheets=[weasyprint.CSS(
+            settings.STATIC_ROOT + 'css/pdf.css')])
     return response
