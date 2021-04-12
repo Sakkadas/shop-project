b7ed93fc753c4a149656ec328a3e97919dc4463a
IlyaNesterov <Eildos95@gmail.com>
2021-04-12 22:40:55 +0300

    Updated

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -5,15 +5,15 @@ from parler.models import TranslatableModel, TranslatedFields
 
 class Category(TranslatableModel):
     translations = TranslatedFields(
-        name=models.CharField(max_length=200,
-                              db_index=True),
-        slug=models.SlugField(max_length=200,
-                              db_index=True,
-                              unique=True)
+        name = models.CharField(max_length=200,
+                                db_index=True),
+        slug = models.SlugField(max_length=200,
+                                db_index=True,
+                                unique=True)
     )
 
     class Meta:
-        #ordering = ('name',)
+        # ordering = ('name',)
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
@@ -21,32 +21,33 @@ class Category(TranslatableModel):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('shop:product_list_by_category', args=[self.slug])
+        return reverse('shop:product_list_by_category',
+                       args=[self.slug])
 
 
 class Product(TranslatableModel):
     translations = TranslatedFields(
-        name=models.CharField(max_length=200, db_index=True),
-        slug=models.SlugField(max_length=200, db_index=True),
-        description=models.TextField(blank=True)
+        name = models.CharField(max_length=200, db_index=True),
+        slug = models.SlugField(max_length=200, db_index=True),
+        description = models.TextField(blank=True)
     )
-    category = models.ForeignKey(
-        Category, related_name='products', on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category,
+                                 related_name='products',
+                                 on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',
+                              blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     # class Meta:
-    #     ordering = ('name',)
-    #     index_together = (('id', 'slug'),)
+    #    ordering = ('name',)
+    #    index_together = (('id', 'slug'),)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('shop:product_detail', args=[self.id, self.slug])
+        return reverse('shop:product_detail',
+                       args=[self.id, self.slug])
