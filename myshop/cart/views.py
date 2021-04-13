0e923b7ec8bbf0ac0e5da15e2e9a88e6e71841e4
IlyaNesterov <Eildos95@gmail.com>
2021-04-13 20:21:19 +0300

    Imported Recommender

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from shop.models import Product
+from shop.recommender import Recommender
+from coupons.forms import CouponApplyForm
 from .cart import Cart
 from .forms import CartAddProductForm
-from coupons.forms import CouponApplyForm
+
 
 @require_POST
 def cart_add(request, product_id):
@@ -29,7 +31,16 @@ def cart_remove(request, product_id):
 def cart_detail(request):
     cart = Cart(request)
     for item in cart:
-        item['update_quantity_form'] = CartAddProductForm(
-            initial={'quantity': item['quantity'], 'override': True})
+        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'],
+                                                                   'override': True})
     coupon_apply_form = CouponApplyForm()
-    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form})
+
+    r = Recommender()
+    cart_products = [item['product'] for item in cart]
+    recommended_products = r.suggest_products_for(cart_products,
+                                                  max_results=4)
+
+    return render(request, 'cart/detail.html',
+                           {'cart': cart,
+                            'coupon_apply_form': coupon_apply_form,
+                            'recommended_products': recommended_products})
